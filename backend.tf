9471c93b467061e3a647531a4e92423528e6c4c1
Yosh <yosephnainggolan1945@gmail.com>
2025-08-01 16:32:20 +0700

    FIX github-actions workflow and backend configuration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                      = "terraform-nainggolan"
-    key                         = "env/terraform.tfstate"
+    key                         = "terraform.tfstate"
     region                      = "auto"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
