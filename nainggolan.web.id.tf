206f2385c4e0aa324476fe4b007f5028b0d4134d
Yosh <yosephnainggolan1945@gmail.com>
2025-08-04 14:57:49 +0700

    test add record

--- nainggolan.web.id.tf
+++ nainggolan.web.id.tf
@@ -35,7 +35,7 @@ resource "cloudflare_record" "terraform_managed_resource_43002899d517b864cbf9968
 }
 
 resource "cloudflare_record" "test_record" {
-  name    = "testt"
+  name    = "test"
   proxied = false 
   ttl     = 1
   type    = "TXT"
