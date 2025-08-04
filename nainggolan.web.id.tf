9f2a1ae99ba4842c9475d546f0a1929a8092ce5a
Yosh <yosephnainggolan1945@gmail.com>
2025-08-04 20:54:09 +0700

    test add record

--- nainggolan.web.id.tf
+++ nainggolan.web.id.tf
@@ -34,3 +34,13 @@ resource "cloudflare_record" "terraform_managed_resource_43002899d517b864cbf9968
   value   = "\"v=spf1 include:zohomail.com ~all\""
 }
 
+resource "cloudflare_record" "test_record" {
+  zone_id = "52f70a1cb815a41afe62102e1165746a"
+  name    = "test add record"
+  type    = "TXT"
+  ttl     = 1
+  proxied = false
+  value   = "\"Test\""
+}
+
+
